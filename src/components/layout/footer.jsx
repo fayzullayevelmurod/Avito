19f8b4f6a40014a59ea2070f002b116b757fab64
QobilovSardor <note47297@gmail.com>
2024-01-08 20:47:19 +0500

    footer section finished

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -2,9 +2,15 @@ import { Flex, Text } from '@chakra-ui/react'
 
 export const Footer = () => {
 	return (
-		<Flex gap={14} >
-			<Text color='gray.500'>@ 2024 Avito Plus - ИП Иванов Иван Иванович</Text>
-			<Text> ИНН 7729773587</Text>
+		<Flex justifyContent={'space-between'} fontSize='sm' color='gray.500'>
+			<Flex gap={14}>
+				<Text>@ 2024 Avito Plus - ИП Иванов Иван Иванович</Text>
+				<Text>ИНН 7729773587</Text>
+			</Flex>
+			<Flex gap={14}>
+				<Text>Политика конфиденциальности</Text>
+				<Text>Оферта</Text>
+			</Flex>
 		</Flex>
 	)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom'
+
+const theme = extendTheme({
+	fonts: {
+		body: "Inter, sans-serif",
+		heading: "Inter, sans-serif",
+	},
+	fontWeights: {
+		normal: 400,
+		medium: 500,
+		bold: 700,
+	},
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
 				<App />
 			</ChakraProvider>
 		</BrowserRouter>
