dbb1afd4d7624aed2525e69b0e6be33a4245dc19
Baxriddin Chorshanbiyev <baxriddin.dev@gmail.com>
2024-01-23 23:12:51 +0500

    added: privacy page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,36 +1,47 @@
 import { Policy } from "../pages/policy";
-import { Account, Home, NotFoundPage, SubscriptionServices, Instructions } from "../pages";
+import {
+  Account,
+  Home,
+  NotFoundPage,
+  SubscriptionServices,
+  Instructions,
+} from "../pages";
 import { Settings } from "../pages/settings";
+import { Privacy } from "../pages/privacy";
 
 const coreRoutes = [
-	{
-		path: '/',
-		component: Home,
-	},
-	{
-		path: '/account',
-		component: Account,
-	},
-	{
-		path: '/subscription-services',
-		component: SubscriptionServices,
-	},
-	{
-		path: '/user-settings',
-		component: Settings,
-	},
-	{
-		path: '/video-instructions',
-		component: Instructions,
-	},
-	{
-		path: '/policy',
-		component: Policy,
-	},
-	{
-		path: '*',
-		component: NotFoundPage
-	}
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/account",
+    component: Account,
+  },
+  {
+    path: "/subscription-services",
+    component: SubscriptionServices,
+  },
+  {
+    path: "/user-settings",
+    component: Settings,
+  },
+  {
+    path: "/video-instructions",
+    component: Instructions,
+  },
+  {
+    path: "/policy",
+    component: Policy,
+  },
+  {
+    path: "/privacy",
+    component: Privacy,
+  },
+  {
+    path: "*",
+    component: NotFoundPage,
+  },
 ];
 
 const routes = [...coreRoutes];
